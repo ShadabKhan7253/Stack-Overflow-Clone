2db29dfed054d3c5313bdae3411f8eec3ba47432
Shadab Khan <shadabkhan7253@gmail.com>
2023-06-01 18:01:36 +0530

    binded the slug to the url

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Question;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -26,6 +27,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Route::bind('slug',function($slug) {
+            return Question::where('slug',$slug)->firstOrFail();
+        });
+
         $this->configureRateLimiting();
 
         $this->routes(function () {
