cbfac37484a9fc1cbf9f5525c43286db325db172
Shadab Khan <shadabkhan7253@gmail.com>
2023-06-01 16:26:34 +0530

    resourcefull route created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\QuestionsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/questions', [App\Http\Controllers\QuestionsController::class, 'index']);
+Route::resource('/questions', QuestionsController::class);
