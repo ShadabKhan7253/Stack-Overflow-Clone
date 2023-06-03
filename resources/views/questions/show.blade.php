e03e5d0abafe7e82a2c548656b196e61d32ea181
Shadab Khan <shadabkhan7253@gmail.com>
2023-06-03 18:20:19 +0530

    section change to push

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
-@section('styles')
+@push('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
         integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-@endsection
+@endpush
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -50,7 +50,7 @@
                 </div>
             </div>
         </div>
-
+        @include('answers._create')
         @include('answers._index')
     </div>
 @endsection
